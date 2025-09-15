6b3b5d7ed7ca1d19eaa03c53f393049c93d1243e
SantiagoBrie <155693553+SantiagoBrie@users.noreply.github.com>
2025-09-15 12:04:50 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import eslint from '@eslint/js';
+// import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  // eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      // ⚡ reglas que realmente te ayudan a evitar bugs
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      // ⚡ Prettier solo como advertencia
+      'prettier/prettier': 'warn',
+    },
+  },
+);
